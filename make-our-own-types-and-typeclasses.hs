b4c060f59761df17ec40f90e7926e4b8dd3ff15b
lancema <messi_18@live.cn>
2016-09-13 22:53:06 +0800

    add Point data type

--- make-our-own-types-and-typeclasses.hs
+++ make-our-own-types-and-typeclasses.hs
@@ -1,11 +1,16 @@
 -- data type is similar to case class in Scala
-data Shape = Rectangle Float Float Float Float | Circle Float Float Float
+data Shape = Rectangle Point Point | Circle Point Float deriving (Show)
 
 -- Value constructor such as Rectancle should not be used in function declaration
 surface :: Shape -> Float 
 
 -- Value constructor should be used in pattern match. Here function definition also is similar in Clojure.
-surface (Circle _ _ r) = pi * r ^ 2 
+surface (Circle _ r) = pi * r ^ 2 
 
 -- dollar usage?
-surface (Rectangle s0 s1 t0 t1) = (abs $ t0 - s0) * (abs $ t1 - s1)
+surface (Rectangle p0 p1) = distance p0 p1
+
+
+data Point = Point Float Float deriving (Show)
+distance :: Point -> Point -> Float
+distance (Point x1 y1) (Point x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
