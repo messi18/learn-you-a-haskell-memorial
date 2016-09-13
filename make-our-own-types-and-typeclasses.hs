cf89b00c7711ff36532014c7141cbc1825088f37
lancema <messi_18@live.cn>
2016-09-13 22:39:49 +0800

    init

--- make-our-own-types-and-typeclasses.hs
+++ make-our-own-types-and-typeclasses.hs
@@ -1,5 +1,11 @@
--- data type is similar to case class in Sclala
+-- data type is similar to case class in Scala
 data Shape = Rectangle Float Float Float Float | Circle Float Float Float
-surface :: Shape -> Float -- Value constructor such as Rectancle should not be used in function declaration
-surface (Circle _ _ r) = pi * r ^ 2 -- Value constructor should be used in pattern match. Here function definition also is similar in clojure.
-surface (Rectangle s0 s1 t0 t1) = (abs $ t0 - s0) * (abs $t1 - s1)
+
+-- Value constructor such as Rectancle should not be used in function declaration
+surface :: Shape -> Float 
+
+-- Value constructor should be used in pattern match. Here function definition also is similar in Clojure.
+surface (Circle _ _ r) = pi * r ^ 2 
+
+-- dollar usage?
+surface (Rectangle s0 s1 t0 t1) = (abs $ t0 - s0) * (abs $ t1 - s1)
